c549cd1b64f13b36e0a510fa8a45ca3ab97e52ac
NayyKo <djebarlen.tambon@gmail.com>
2023-10-28 10:17:16 +0400

    Modification du bouton search / Création formations.json

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,20 +4,20 @@ import SearchModal from "./SearchModal/SearchModal";
 export default function Navbar(){
     return(
         <>
-        <nav className="sticky top-0 border-gray-200 bg-pink-900 shadow-lg dark:bg-gray-800 dark:border-gray-700">
+        <nav className="sticky top-0 bg-gradient-to-r to-pink-600 from-red-400 shadow-lg dark:bg-gray-800 dark:border-gray-700">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <NavLink to="/" className="flex flex-col justify-center items-center">
                 <img src={Logo} className="h-12" alt="May Logo" />
                 <span className="text-xl font-extrabold text-white">May Formations</span>
             </NavLink>
-            <button data-collapse-toggle="navbar-solid-bg" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-solid-bg" aria-expanded="false">
+            <button data-collapse-target="navbar-solid-bg" data-collapse-toggle="navbar-solid-bg" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-solid-bg" aria-expanded="false">
                 <span className="sr-only">Ouvrir le menu</span>
                 <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
                 </svg>
             </button>
             <div className="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
-            <ul className="flex flex-col font-light text-white mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+            <ul className="flex flex-col items-center font-light text-white mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
                 <li>
                     <NavLink to="/" className="block font-bold py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-white md:p-0 md:dark:text-pink-500 dark:bg-blue-600 md:dark:bg-transparent">Home</NavLink>
                 </li>
--- src/components/Navbar/SearchModal/SearchModal.js
+++ src/components/Navbar/SearchModal/SearchModal.js
@@ -2,22 +2,31 @@ import { useState } from "react";
 
 export default function SearchModal(){
     const [showModal, setShowModal] = useState(false);
+    const [searchTerm, setSearchTerm] = useState();
+    const handleChange = (e) => {
+        console.log(e.target.value);
+        setSearchTerm(e.target.value);
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(searchTerm);
+    }
     return(
         <>
         <button className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" onClick={() => setShowModal(true)}>
-            Open regular modal
+            <i className="fa-solid fa-search"></i>
         </button>
         {showModal ? (
             <>
             <div
                 className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
             >
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+            <div className="relative w-full my-6 mx-auto max-w-6xl ">
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-                  <h3 className="text-3xl text-slate-900 font-semibold">
+                <div className="flex items-start justify-between p-8 rounded-t">
+                  <h3 className="text-3xl md:text-5xl text-slate-700 font-black">
                     Que recherchez-vous ?
                   </h3>
                   <button
@@ -31,12 +40,14 @@ export default function SearchModal(){
                 </div>
                 {/*body*/}
                 <div className="relative p-6 flex-auto">
-                  <form>
-                    <div>
+                  <form className="flex items-center">
+                    <div className="w-full">
                         <label htmlFor="SearchQuery"></label>
-                        <input type="text" id="large-input" class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+                        <input type="text" id="large-input" value={searchTerm} onChange={handleChange} name="search_query" className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
                     </div>
-                    <button type="submit" className="border border-pink-300 text-pink-300 p-3 mt-5 ">Recherche</button>
+                    <button type="button" onClick={handleSubmit} className="relative border border-pink-300 text-pink-300 p-4 px-5 rounded-lg shadow hover:shadow-lg duration-300  ">
+                        <i className="fa-solid fa-search"></i>
+                    </button>
                   </form>
                 </div>
                 {/*footer*/}
